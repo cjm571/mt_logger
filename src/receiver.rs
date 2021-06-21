78e32ab52c32a7c746c0089019fa1b339d38c1a5
CJ McAllister <cjm571@gmail.com>
2021-06-20 23:17:47 -0600

    Implemented flush() and mt_flush!() - Added Command::Flush - Added MtLoggerError::RecvError - Serialized tests (temporarily?) - Replaced sleep-waits with flushes in tests

--- src/receiver.rs
+++ src/receiver.rs
@@ -23,13 +23,14 @@ Purpose:
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::{mpsc, Arc};
 
-use std::fs;
+use std::fs::{self, File};
 use std::io::prelude::*;
 use std::path::PathBuf;
 
 use chrono::Local;
+use chrono::format::{DelayedFormat, StrftimeItems};
 
-use crate::{Command, Level, OutputStream};
+use crate::{Command, Level, MsgTuple, OutputStream};
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -110,6 +111,7 @@ impl Receiver {
         }
 
         path_buf.push(logfile_name);
+        //OPT: *DESIGN* Could this be a member of Receiver?
         let mut logfile = match fs::File::create(path_buf.as_path()) {
             Ok(file) => file,
             Err(err) => panic!(
@@ -145,85 +147,7 @@ impl Receiver {
                 match logger_cmd {
                     /* Messages */
                     Command::LogMsg(log_tuple) => {
-                        if log_tuple.level >= self.output_level {
-                            // Console output
-                            if self.output_stream as u8 & OutputStream::StdOut as u8 != 0 {
-                                let log_color = match log_tuple.level {
-                                    Level::Trace => "\x1b[030;105m",
-                                    Level::Debug => "\x1b[030;106m",
-                                    Level::Info => "\x1b[030;107m",
-                                    Level::Warning => "\x1b[030;103m",
-                                    Level::Error => "\x1b[030;101m",
-                                    Level::Fatal => "\x1b[031;040m",
-                                };
-                                let msg_formatted = format!(
-                                    "{timestamp}: {color_set}[{level:^level_width$}]\x1b[0m {fn_name}() line {line}:\n{msg:>msg_leftpad$}",
-                                    timestamp   = timestamp,
-                                    color_set   = log_color,
-                                    level       = log_tuple.level.to_string(),
-                                    level_width = LEVEL_LABEL_WIDTH,
-                                    fn_name     = log_tuple.fn_name,
-                                    line        = log_tuple.line,
-                                    msg         = log_tuple.msg,
-                                    msg_leftpad = MESSAGE_LEFT_PADDING + log_tuple.msg.len(),
-                                );
-
-                                // Write to console
-                                println!("{}", msg_formatted);
-
-                                #[cfg(test)]
-                                {
-                                    // Add newline to formatted message for readability
-                                    let writeable_msg = format!("{}\n", msg_formatted);
-
-                                    // Write to stdout verification file
-                                    let mut stdout_redirect = fs::OpenOptions::new().append(true).open(STDOUT_FILENAME)
-                                        .unwrap_or_else(
-                                            |err| panic!("Encountered error '{}' while attempting to open stdout verification file.", err)
-                                        );
-                                    stdout_redirect.write_all(writeable_msg.as_bytes())
-                                        .unwrap_or_else(
-                                            |err| panic!("Encountered error '{}' while attempting to write to stdout verification file.", err)
-                                        );
-                                }
-                            }
-
-                            // File output
-                            if self.output_stream as u8 & OutputStream::File as u8 != 0 {
-                                let msg_formatted = format!(
-                                    "{timestamp}: [{level:^level_width$}] {fn_name}() line {line}:\n{msg:>msg_leftpad$}\n",
-                                    timestamp   = timestamp,
-                                    level       = log_tuple.level.to_string(),
-                                    level_width = LEVEL_LABEL_WIDTH,
-                                    fn_name     = log_tuple.fn_name,
-                                    line        = log_tuple.line,
-                                    msg         = log_tuple.msg,
-                                    msg_leftpad = MESSAGE_LEFT_PADDING + log_tuple.msg.len(),
-                                );
-
-                                //FEAT: Avoid spewing the same error if a file explodes or something
-                                logfile.write_all(msg_formatted.as_bytes())
-                                    .unwrap_or_else(
-                                        |err| eprintln!("{}: Encountered error '{}' while attempting to write to log file.", timestamp, err)
-                                    );
-
-                                #[cfg(test)]
-                                {
-                                    // Write to stdout verification file
-                                    let mut file_redirect = fs::OpenOptions::new().append(true).open(FILE_OUT_FILENAME)
-                                        .unwrap_or_else(
-                                            |err| panic!("Encountered error '{}' while attempting to open file output verification file.", err)
-                                        );
-                                    file_redirect.write_all(msg_formatted.as_bytes())
-                                        .unwrap_or_else(
-                                            |err| panic!("Encountered error '{}' while attempting to write to file output verification file.", err)
-                                        )
-                                }
-                            }
-                        }
-
-                        // Increment shared message count
-                        self.msg_count.fetch_add(1, Ordering::SeqCst);
+                        self.record_msg(&mut logfile, timestamp, log_tuple)
                     }
 
                     /* Configuration Commands */
@@ -233,8 +157,114 @@ impl Receiver {
                     Command::SetOutputStream(output_stream) => {
                         self.output_stream = output_stream;
                     }
+
+                    /* Flush */
+                    Command::Flush(flush_ack_tx) => {
+                        // If we're processing this command, all other previous commands have already
+                        // been processed. Simple send the ACK back to the main thread.
+
+                        // Only handle the failure case - nothing to do on success
+                        if let Err(e) = flush_ack_tx.send(()) {
+                            // Write an error into the log so we know something went wrong
+                            let err_tuple = MsgTuple {
+                                level: Level::Error,
+                                fn_name: "LOG_RECEIVER_FLUSH_COMMAND".to_string(),
+                                line: line!(),
+                                msg: format!("Encountered SendError '{}' when sending flush ACK message.", e),
+                            };
+
+                            self.record_msg(&mut logfile, timestamp, err_tuple);
+                        }
+                    }
                 };
             }
         }
+    }
+
+
+    /*  *  *  *  *  *  *\
+     * Helper Methods *
+    \*  *  *  *  *  *  */
+
+    fn record_msg(&mut self, logfile: &mut File, timestamp: DelayedFormat<StrftimeItems>, log_tuple: MsgTuple) {
+        if log_tuple.level >= self.output_level {
+            // Console output
+            if self.output_stream as u8 & OutputStream::StdOut as u8 != 0 {
+                let log_color = match log_tuple.level {
+                    Level::Trace => "\x1b[030;105m",
+                    Level::Debug => "\x1b[030;106m",
+                    Level::Info => "\x1b[030;107m",
+                    Level::Warning => "\x1b[030;103m",
+                    Level::Error => "\x1b[030;101m",
+                    Level::Fatal => "\x1b[031;040m",
+                };
+                let msg_formatted = format!(
+                    "{timestamp}: {color_set}[{level:^level_width$}]\x1b[0m {fn_name}() line {line}:\n{msg:>msg_leftpad$}",
+                    timestamp   = timestamp,
+                    color_set   = log_color,
+                    level       = log_tuple.level.to_string(),
+                    level_width = LEVEL_LABEL_WIDTH,
+                    fn_name     = log_tuple.fn_name,
+                    line        = log_tuple.line,
+                    msg         = log_tuple.msg,
+                    msg_leftpad = MESSAGE_LEFT_PADDING + log_tuple.msg.len(),
+                );
+
+                // Write to console
+                println!("{}", msg_formatted);
+
+                #[cfg(test)]
+                {
+                    // Add newline to formatted message for readability
+                    let writeable_msg = format!("{}\n", msg_formatted);
+
+                    // Write to stdout verification file
+                    let mut stdout_redirect = fs::OpenOptions::new().append(true).open(STDOUT_FILENAME)
+                        .unwrap_or_else(
+                            |err| panic!("Encountered error '{}' while attempting to open stdout verification file.", err)
+                        );
+                    stdout_redirect.write_all(writeable_msg.as_bytes())
+                        .unwrap_or_else(
+                            |err| panic!("Encountered error '{}' while attempting to write to stdout verification file.", err)
+                        );
+                }
+            }
+
+            // File output
+            if self.output_stream as u8 & OutputStream::File as u8 != 0 {
+                let msg_formatted = format!(
+                    "{timestamp}: [{level:^level_width$}] {fn_name}() line {line}:\n{msg:>msg_leftpad$}\n",
+                    timestamp   = timestamp,
+                    level       = log_tuple.level.to_string(),
+                    level_width = LEVEL_LABEL_WIDTH,
+                    fn_name     = log_tuple.fn_name,
+                    line        = log_tuple.line,
+                    msg         = log_tuple.msg,
+                    msg_leftpad = MESSAGE_LEFT_PADDING + log_tuple.msg.len(),
+                );
+
+                //FEAT: Avoid spewing the same error if a file explodes or something
+                logfile.write_all(msg_formatted.as_bytes())
+                    .unwrap_or_else(
+                        |err| eprintln!("{}: Encountered error '{}' while attempting to write to log file.", timestamp, err)
+                    );
+
+                #[cfg(test)]
+                {
+                    // Write to stdout verification file
+                    let mut file_redirect = fs::OpenOptions::new().append(true).open(FILE_OUT_FILENAME)
+                        .unwrap_or_else(
+                            |err| panic!("Encountered error '{}' while attempting to open file output verification file.", err)
+                        );
+                    file_redirect.write_all(msg_formatted.as_bytes())
+                        .unwrap_or_else(
+                            |err| panic!("Encountered error '{}' while attempting to write to file output verification file.", err)
+                        )
+                }
+            }
+        }
+
+        // Increment shared message count
+        self.msg_count.fetch_add(1, Ordering::SeqCst);
     }
 }
